cdd99c0816866566117f28c8d6d0916aff789195
Adrima Biswas <adrimabiswas2003@gmail.com>
2026-08-22 00:19:41 +0530

    Time: 1 ms (99.98%), Space: 43.5 MB (75.97%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,27 +1,42 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stack = new Stack<>();
+        int top = -1;
+        char[] stack = new char[s.length()];
         for(int i = 0; i < s.length(); i++){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i));
-            }else if(stack.peek() == s.charAt(i)){
-                stack.push(s.charAt(i));
-            }else if(stack.peek() == (char)(s.charAt(i) + 32)){
-                stack.pop();
-            }else if(stack.peek() == (char)(s.charAt(i) - 32)){
-                stack.pop();
-            }else if(!stack.isEmpty()){
-                stack.push(s.charAt(i));
+            if(top >= 0 && stack[top] == (char)(s.charAt(i) + 32) ){
+                top--;
+            }else if(top >= 0 && stack[top] == (char)(s.charAt(i) - 32)){
+                top--;
+            }else{
+                top++;
+                stack[top] = s.charAt(i);
             }
         }
 
-        StringBuilder str = new StringBuilder();
+        return new String(stack, 0, top+1);
 
-        while(!stack.isEmpty()){
-            str.append(stack.pop());
-        }
+        // Stack<Character> stack = new Stack<>();
+        // for(int i = 0; i < s.length(); i++){
+        //     if(stack.isEmpty()){
+        //         stack.push(s.charAt(i));
+        //     }else if(stack.peek() == s.charAt(i)){
+        //         stack.push(s.charAt(i));
+        //     }else if(stack.peek() == (char)(s.charAt(i) + 32)){
+        //         stack.pop();
+        //     }else if(stack.peek() == (char)(s.charAt(i) - 32)){
+        //         stack.pop();
+        //     }else if(!stack.isEmpty()){
+        //         stack.push(s.charAt(i));
+        //     }
+        // }
 
-        String ans = str.reverse().toString();
-        return ans;
+        // StringBuilder str = new StringBuilder();
+
+        // while(!stack.isEmpty()){
+        //     str.append(stack.pop());
+        // }
+
+        // String ans = str.reverse().toString();
+        // return ans;
     }
 }

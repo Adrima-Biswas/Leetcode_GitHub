7520f4f240eba7b54ea6eb1dbb53bfe6e222af31
Adrima Biswas <adrimabiswas2003@gmail.com>
2026-08-23 23:47:48 +0530

    Time: 38 ms (81.18%), Space: 132.8 MB (7.57%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        for(int i = 0; i < temperatures.length; i++){
+
+            while(top >= 0 && temperatures[stack[top]] < temperatures[i] ){ // Checking to maintain Monotonic stack
+                // pop 
+                map.put(stack[top], i - stack[top] );
+                top--;
+            }
+            // Now u have to push the data
+            top++;
+            stack[top] = i;         
+        }
+
+        while(top >= 0){ // for remaining data into stack
+            map.put(stack[top--], 0);
+        }
+
+        for(int i = 0; i < temperatures.length; i++){
+            temperatures[i] = map.get(i);
+        }
+        return temperatures;
+    }
+}
